794d10cb5bb4838a09a0db96de36d6fd69835a87
DmPochapskyi <dmpochapskyj@gmail.com>
2024-11-24 19:55:22 +0100

    monitoring added

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, Response, request
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return jsonify({"message": "Hello, World!"})
+# Визначення метрики для підрахунку запитів
+REQUEST_COUNT = Counter('app_requests_total', 'Total HTTP requests', ['method', 'endpoint'])
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.before_request
+def before_request_func():
+    # Лічильник HTTP-запитів
+    REQUEST_COUNT.labels(method=request.method, endpoint=request.path).inc()
+
+@app.route('/')
+def hello_world():
+    return {"message": "Hello, World!"}
+
+@app.route('/metrics')
+def metrics():
+    # Генерація метрик у форматі Prometheus
+    return Response(generate_latest(), mimetype="text/plain")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
